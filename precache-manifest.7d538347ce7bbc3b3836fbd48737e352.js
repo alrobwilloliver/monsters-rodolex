d3a12634b38b66c8b5531ab197859f376ffd6eed
Alan Oliver <alrobwilloliver@gmail.com>
2020-09-06 15:55:09 +0100

    Updates

---
+++ precache-manifest.7d538347ce7bbc3b3836fbd48737e352.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6550632ca40e197c30d7afa4ff66a04",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9ef94a79a4b02383d983",
+    "url": "/monsters-rolodex/static/css/main.6c2193a2.chunk.css"
+  },
+  {
+    "revision": "c7d7319a71b6cb93be4e",
+    "url": "/monsters-rolodex/static/js/2.73d70fc7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.73d70fc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ef94a79a4b02383d983",
+    "url": "/monsters-rolodex/static/js/main.f45da2e5.chunk.js"
+  },
+  {
+    "revision": "7707380ad873a5d4df59",
+    "url": "/monsters-rolodex/static/js/runtime-main.c1dd9bd2.js"
+  }
+]);
