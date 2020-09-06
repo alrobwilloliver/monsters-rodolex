efeecae6278e80a0ed6f5491a77537ec632e59b6
Alan Oliver <alrobwilloliver@gmail.com>
2020-09-06 14:49:37 +0100

    added title and styling

--- src/App.js
+++ src/App.js
@@ -16,6 +16,10 @@ class App extends Component {
 
   }
 
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value })
+  }
+
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
@@ -28,9 +32,10 @@ class App extends Component {
     const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
     return (
       <div className="App">
+        <h1>Monsters Rolodex</h1>
         <SearchBox
           placeholder="Search Monsters"
-          handleChange={e => this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters}>
         </CardList>
